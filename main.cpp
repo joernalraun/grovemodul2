4417d385c843218a24183bb528e2b5e925f1fd1f
Juri Wolf <Amerlander@users.noreply.github.com>
2025-03-19 12:08:44 +0100

    Refactor isCodal function and simplify board version logic in main.ts

--- main.cpp
+++ main.cpp
@@ -1,24 +1,19 @@
 #include "pxt.h"
 
 namespace grove {
-
-#if MICROBIT_CODAL
-    #define isCodal true
-#else
-    #define isCodal false
-#endif
-
     /**
-     * Returns if it is Codal
-     * 
+     * Return true if current environment is Codal
      */
-    //% help=basic/pause weight=50
     //% async block="isCodal" blockGap=16
     //% blockId=grove_is_codal
     //% group="grove"
-    //% hidden=1 deprecated=1
+
     bool isCodal() {
-        return isCodal;
+#if MICROBIT_CODAL
+       return true;
+#else
+       return false;
+#endif
     }
 
 }
